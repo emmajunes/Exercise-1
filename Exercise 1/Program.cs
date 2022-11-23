82cd6a5337de6c6aa096422b118667b8588212e5
DESKTOP-9FQ8PPR\Emma Junes <emmaastrom91@gmail.com>
2022-11-23 12:35:16 +0100

    Add project files.

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exercise_1
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var stopwatch = new Stopwatch();
+            
+
+            while (true)
+            {
+                Console.WriteLine("Tap 'y' to start, 'n' to stop, or 'q' to quit.");
+                //var character = Console.ReadLine();
+
+                
+                switch (Console.ReadLine())
+                {
+                    case "y":
+                            Console.WriteLine("start");
+                            stopwatch.Start(); 
+                   
+                        break;
+                    case "n":
+                        Console.WriteLine("stop");
+                        Console.WriteLine(stopwatch.Stop());
+                       
+                        break;
+                    case "q":
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ Exercise 1/Stopwatch.cs
@@ -0,0 +1,46 @@
+﻿namespace Exercise_1
+{
+    internal partial class Program
+    {
+        public class Stopwatch
+        {
+            private DateTime _start;
+            private DateTime _stop;
+            private bool _running;
+            public void Start()
+            {
+             
+                if (!_running)
+                {
+                    _running = true;
+                    _start = DateTime.Now;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Stopwatch is already running!");
+                }
+                    
+            }
+
+            public TimeSpan Stop()
+            {
+
+                if (_running)
+                {
+                    _running = false;
+                    _stop = DateTime.Now;
+                    
+                }
+                else
+                {
+                    throw new InvalidOperationException("Stopwatch is already running!");
+                }
+                    
+
+                return (_stop - _start);
+            }
+
+
+        }
+    }
+}
